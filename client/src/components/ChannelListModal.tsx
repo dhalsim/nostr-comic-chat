d9753d4375ce62f6467ff22681ae7408709247be
Baris Aydek <baris.aydek@gmail.com>
2025-06-07 14:02:59 +0300

    fixed zindex issue with the channel modal

--- client/src/components/ChannelListModal.tsx
+++ client/src/components/ChannelListModal.tsx
@@ -175,7 +175,7 @@ export const ChannelListModal = ({
 
   return (
     <div
-      className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center"
+      className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50"
       onClick={(e) => {
         if (e.target === e.currentTarget) {
           onClose();
